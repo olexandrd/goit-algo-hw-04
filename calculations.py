226de886ebb217f2dad50e3267d61e1cd1db4037
Olexandrd <olexandrd@gmail.com>
2024-01-02 22:04:06 +0200

    calulations

--- calculations.py
+++ calculations.py
@@ -1,38 +1,7 @@
 import time
 import matplotlib.pyplot as plt
 from data_generator import generate_array
-from sorts import insertion_sort, tim_sort
-
-def merge_sort(arr) -> list:
-    if len(arr) <= 1:
-        return arr
-    mid = len(arr) // 2
-    left_half = arr[:mid]
-    right_half = arr[mid:]
-    return merge(merge_sort(left_half), merge_sort(right_half))
-
-
-def merge(left, right) -> list:
-    merged = []
-    left_index = 0
-    right_index = 0
-    # Спочатку об'єднайте менші елементи
-    while left_index < len(left) and right_index < len(right):
-        if left[left_index] <= right[right_index]:
-            merged.append(left[left_index])
-            left_index += 1
-        else:
-            merged.append(right[right_index])
-            right_index += 1
-    # Якщо в лівій або правій половині залишилися елементи,
-    # додайте їх до результату
-    while left_index < len(left):
-        merged.append(left[left_index])
-        left_index += 1
-    while right_index < len(right):
-        merged.append(right[right_index])
-        right_index += 1
-    return merged
+from sorts import insertion_sort, merge, merge_sort, tim_sort
 
 # We do not need to generate random lists every time for measures
 memo = dict()
@@ -49,7 +18,7 @@ def collect_execution_time(method, datasize=10000):
     return execution_time
 
 # Define size for sorting
-n = [2**x for x in range(1, 6)]
+n = [2**x for x in range(4, 27)]
 
 # Get execution time for each sort algorithm
 timSort = [collect_execution_time(tim_sort, x) for x in n]
